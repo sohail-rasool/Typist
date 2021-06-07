2d622263444cf783c97274242a76c57760eb647a
sohail Rasool <sohailrasoolabbasi21@gmail.com>
2021-06-07 23:03:50 +0500

    Project Added in repo

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
+AOS.init();
